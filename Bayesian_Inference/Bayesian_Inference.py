67c0edaba5f37ee89f9e3fe0008eb2b418179064
HONG <soojung.hong@atos.net>
2017-10-16 16:21:40 +0200

    Add 100 previous campaigns in the past

--- Bayesian_Inference/Bayesian_Inference.py
+++ Bayesian_Inference/Bayesian_Inference.py
@@ -7,6 +7,9 @@ Created on Mon Oct 16 09:39:10 2017
 Reference : https://www.datascience.com/blog/introduction-to-bayesian-inference-learn-data-science-tutorials
 
 """
+#--------------------------------------
+# Likelihood of Theta of sinlge run 
+#--------------------------------------
 
 import numpy as np
 from scipy.misc import factorial
@@ -50,4 +53,46 @@ ax.set_xlabel("Theta")
 ax.set_ylabel("Likelihood")
 ax.grid()
 ax.set_title("Likelihood of Theta for New Campaign")
+plt.show()
+
+
+#------------------------------------------------
+# Overlay Likelihood with previous 100 campaign 
+#------------------------------------------------
+plt.rcParams['figure.figsize'] = (10, 5) # (16, 7)
+import numpy as np
+#import pandas as pd
+
+true_a = 11.5
+true_b = 48.5
+
+#number of marketing campaigns
+N = 100
+
+#randomly generate "true" click through rate for each campaign
+p = np.random.beta(true_a,true_b, size=N) #beta distribution
+
+#randomly pick the number of impressions for each campaign
+impressions = np.random.randint(1, 10000, size=N)
+
+#sample number of clicks for each campaign
+clicks = np.random.binomial(impressions, p).astype(float)
+click_through_rates = clicks / impressions
+#plot the histogram of previous click through rates with the evidence#of the new campaign
+f, ax = plt.subplots(1)
+ax.axvline(mle, linestyle = "--")
+ax.plot(possible_theta_values, likelihoods)
+
+zero_to_one = [j/100. for j in xrange(100)]
+counts, bins = np.histogram(click_through_rates
+                            , bins=zero_to_one)
+counts = counts / 100.
+ax.plot(bins[:-1],counts, alpha = .5)
+line1, line2, line3 = ax.lines
+ax.legend((line2, line3), ('Likelihood of Theta for New Campaign'
+                           , 'Frequency of Theta Historically')
+                          , loc = 'upper left')
+ax.set_xlabel("Theta")
+ax.grid()
+ax.set_title("Evidence vs Historical Click Through Rates")
 plt.show()
