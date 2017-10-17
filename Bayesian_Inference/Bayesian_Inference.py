702b335cd161b6c852519cd4bf3069d99b488b4f
HONG <soojung.hong@atos.net>
2017-10-17 17:17:17 +0200

    Fit beta distribution along with estimated prior distribution with previous ctr

--- Bayesian_Inference/Bayesian_Inference.py
+++ Bayesian_Inference/Bayesian_Inference.py
@@ -78,6 +78,7 @@ impressions = np.random.randint(1, 10000, size=N)
 #sample number of clicks for each campaign
 clicks = np.random.binomial(impressions, p).astype(float)
 click_through_rates = clicks / impressions
+
 #plot the histogram of previous click through rates with the evidence#of the new campaign
 f, ax = plt.subplots(1)
 ax.axvline(mle, linestyle = "--")
@@ -95,4 +96,44 @@ ax.legend((line2, line3), ('Likelihood of Theta for New Campaign'
 ax.set_xlabel("Theta")
 ax.grid()
 ax.set_title("Evidence vs Historical Click Through Rates")
+plt.show()
+
+#--------------------------------------------------------------------------------------------------------------
+# fit the beta distribution and compare the estimated prior distribution with previous click-through rates 
+# to ensure the two are properly aligned:
+#--------------------------------------------------------------------------------------------------------------
+from scipy.stats import beta
+
+#fit beta to previous CTRs
+prior_parameters = beta.fit(click_through_rates
+                            , floc = 0
+                            , fscale = 1)
+#extract a,b from fit
+prior_a, prior_b = prior_parameters[0:2]
+
+#define prior distribution sample from prior
+prior_distribution = beta(prior_a, prior_b)
+
+#get histogram of samples
+prior_samples = prior_distribution.rvs(10000)
+
+#get histogram of samples
+fit_counts, bins = np.histogram(prior_samples, zero_to_one)
+
+#normalize histogram
+fit_counts = map(lambda x: float(x)/fit_counts.sum(), fit_counts)
+
+#plot
+f, ax = plt.subplots(1)
+ax.plot(bins[:-1], fit_counts)
+
+hist_ctr, bins = np.histogram(click_through_rates, zero_to_one)
+hist_ctr = map(lambda x: float(x)/hist_ctr.sum(), hist_ctr)
+ax.plot(bins[:-1], hist_ctr)
+estimated_prior, previous_click_through_rates = ax.lines
+ax.legend((estimated_prior, previous_click_through_rates)
+          ,('Estimated Prior'
+            , 'Previous Click Through Rates'))
+ax.grid()
+ax.set_title("Comparing Empirical Prior with Previous Click Through Rates")
 plt.show()
